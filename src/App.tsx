ae161a3085ad8564b0e63ec7580c72f19160cbb0
APIDOPOULOS Konstantinos <kapidopoulos@netcompany-intrasoft.com>
2022-12-28 14:18:05 +0200

    test husky rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Linting & Formatting
         </a>
       </header>
     </div>
